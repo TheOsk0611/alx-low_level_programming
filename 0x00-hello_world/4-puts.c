0e89d42d31a196f847a46d539318c2836b6740ac
Julio Vilaboa <vilaboa.julio@gmail.com>
2022-06-02 19:27:36 -0700

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main -entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+  puts("\"Programming is like building a multibilingual puzzle");
+  return (0);
+
+}
+
