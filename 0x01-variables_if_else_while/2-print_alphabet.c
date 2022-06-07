a93eb8abdfc67b8f20649f566d9a7e0c4ad6839a
Julio Vilaboa <vilaboa.julio@gmail.com>
2022-06-06 18:05:07 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stlib.h>
+
+/**
+ *main -entry point
+ *
+ *Return:0
+ */
+int main(void)
+{
+char a = 'a';
+while ( a<='z')
+   {
+     putchar(a);
+     a++
+   }
+ purchar('\n');
+
+ return(0);
+}
