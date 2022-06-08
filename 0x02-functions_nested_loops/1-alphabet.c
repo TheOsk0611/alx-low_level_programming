b4f11ba8fb706804e403a5c35680187a1146e1d0
Julio Vilaboa <vilaboa.julio@gmail.com>
2022-06-07 18:22:24 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ *
+ *print_alphabet -a function printsthe alphabet in lovercase
+ *
+ *Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+int n;
+
+for (n = 'a'; n <= 'z'; n++)
+{
+_putchar(n);
+}
+_putchar('\n');
+
+}
