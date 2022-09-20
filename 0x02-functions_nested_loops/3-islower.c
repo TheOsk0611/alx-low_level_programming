01db7019ee0d0144b3805943e0b0f500e4b8983c
julio <julio@gmail.com>
2022-09-20 07:23:32 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*_islower - return 1 or 0 lowercase
+*@c: char type letter
+*Return: 1 (true) 0 (false)
+*/
+int _islower(int c)
+{
+if(c > 96 && c <123)
+return (1);
+else
+return (0);
+}
