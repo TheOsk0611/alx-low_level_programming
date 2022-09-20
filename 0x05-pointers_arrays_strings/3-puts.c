493145015ef28315981c5e11a87711ccf1714c52
julio <julio@gmail.com>
2022-09-20 00:57:06 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*_puts - print string to stout
+*@str: char array string type
+*Description: only _putchar
+*Return: Nothing
+*/
+void _puts(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
