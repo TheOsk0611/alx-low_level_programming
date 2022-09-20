2116d2b13a204b2f770d6f5a1ac9afd5793dfd11
julio <julio@gmail.com>
2022-09-20 01:14:28 -0700

    puts

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
-*strlen - return lenght of string
+*_strlen - return lenght of string
 *@s: char type - pointer
 *Return: String's lenght
 */
+
 int _strlen(char *s)
 {
 int len;
 while (*s != '\0')
-  {
-    len += 1;
-    *s = *s + 1;
-  }
-
+{
+len += 1;
+*s = *s + 1;
+}
 return (len);
 }
