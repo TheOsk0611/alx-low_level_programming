3129a6eb45398d6af6fb43c2ed2df2624dd9eeee
julio <julio@gmail.com>
2022-09-20 12:03:35 -0700

    8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* jack_bauer - prints 24 hour clock
+*h = hour, m = min
+*breaks before passing 24:00
+*Return: 24 hour
+*/
+int jack_bauer(void)
+{
+int h, m;
+for (h = 0; h < 24; h++)
+{
+_putchar((h / 10) + '0');
+_putchar((h % 10) + '0');
+_putchar(':');
+_putchar((m / 10) + '0');
+_putchar((m % 10) + '0');
+_putchar('\n');
+}
+
+}
+
+}
