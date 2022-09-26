f00393ed77bd802de46adebc8411d7e372fa8c40
julio <julio@gmail.com>
2022-09-26 07:59:23 -0700

    1

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,15 +5,16 @@
 * @s: memory area to be filled
 * @b: char to copy
 * @n: numbers of time to copy b
-* Return: pointer to the memory area s
+* Return: s
 */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int i;
-for (i = 0, i < n; i++)
+unsigned int index;
+for (index = 0, n > 0; index++)
 {
-s[i] = b;
+s[index] = b;
+n -=1;
 }
 return (s);
 
