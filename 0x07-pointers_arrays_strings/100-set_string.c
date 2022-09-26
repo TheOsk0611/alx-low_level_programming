00ec92c3f341963784048f79b1ea1bc290f747e4
julio <julio@gmail.com>
2022-09-26 11:59:42 -0700

    8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*set_string - pointer to string
+*@s: pointer to pointer to char
+*@to: the address to match
+*Return: void
+*/
+
+void set_string(char **s, char *to)
+
+{
+*s = to;
+}
