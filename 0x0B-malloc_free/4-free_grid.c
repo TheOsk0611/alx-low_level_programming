f3b8a66cf2246f931f20eaa3c04b0d8d31a2448d
julio <julio@gmail.com>
2022-10-04 19:41:47 -0700

    4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*free_grid - function to allocate memory grid
+*@grid: int arg
+*@height: int arg
+*Return: grid of 0s
+*/
+void free_grid(int **grid, int height)
+{
+int i;
+for (i = 0; i < height; ++i)
+{
+free(grid[i]);
+}
+free(grid);
+}
