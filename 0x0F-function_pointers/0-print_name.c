fb5c06bffc8f55fcdb8ef80b785560cdf0f1d3c1
julio <julio@gmail.com>
2022-10-12 18:47:15 -0700

    check

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 /**
- * print_name - write a funtion that print a name
- *@name: char pointer
- *@f: pointer
- *Return: void
- */
+*print_name - write a funtion that print a name
+*@name: char pointer
+*@f: pointer
+*Return: void
+*/
 void print_name(char *name, void (*f)(char *))
 {
 if (name && f)
